99139efab71e365cc1a64817e937b926fc263829
Christopher House <chhouse@microsoft.com>
2020-09-15 10:22:47 -0500

    added beer style apis + swagger

--- MyBeerCellar.API/Startup.cs
+++ MyBeerCellar.API/Startup.cs
@@ -26,11 +26,19 @@ namespace MyBeerCellar.API
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
+            services.AddSwaggerGen();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
+            app.UseSwagger();
+
+            app.UseSwaggerUI(_ =>
+            {
+                _.SwaggerEndpoint("/swagger/v1/swagger.json", "MyBeerCellar API v1");
+            });
+
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();

e4dda730b340cc1f579d971cd1364a4b3f6442d0
Christopher House <chhouse@microsoft.com>
2020-09-19 10:54:43 -0500

    Added appconfig stuff

---
+++ MyBeerCellar.API/Constants.cs
@@ -0,0 +1,10 @@
+namespace MyBeerCellar.API
+{
+    public static class Constants
+    {
+        public static class ConfigurationKeys
+        {
+            public const string AppConfigConnectionStringKey = "ConnectionStrings:AppConfig";
+        }
+    }
+}
--- MyBeerCellar.API/Program.cs
+++ MyBeerCellar.API/Program.cs
@@ -1,11 +1,6 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
 
 namespace MyBeerCellar.API
 {
@@ -16,11 +11,12 @@ namespace MyBeerCellar.API
             CreateHostBuilder(args).Build().Run();
         }
 
-        public static IHostBuilder CreateHostBuilder(string[] args) =>
-            Host.CreateDefaultBuilder(args)
-                .ConfigureWebHostDefaults(webBuilder =>
+        public static IHostBuilder CreateHostBuilder(string[] args) => Host.CreateDefaultBuilder(args)
+            .ConfigureWebHostDefaults(_ => _.ConfigureAppConfiguration((hostingContext, config) =>
                 {
-                    webBuilder.UseStartup<Startup>();
-                });
+                    var settings = config.Build();
+                    config.AddAzureAppConfiguration(settings[Constants.ConfigurationKeys.AppConfigConnectionStringKey]);
+                })
+                .UseStartup<Startup>());
     }
 }

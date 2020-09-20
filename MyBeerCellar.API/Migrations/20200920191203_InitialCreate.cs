d9f2ee6acdb5ed146d4dcd4717d69ac5ec57224a
Christopher House <chhouse@microsoft.com>
2020-09-20 14:12:59 -0500

    Added initial migrations

---
+++ MyBeerCellar.API/Migrations/20200920191203_InitialCreate.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace MyBeerCellar.API.Migrations
+{
+    public partial class InitialCreate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "BeerStyle",
+                columns: table => new
+                {
+                    StyleId = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    StyleName = table.Column<string>(type: "nvarchar", maxLength: 255, nullable: false),
+                    DateCreated = table.Column<DateTime>(nullable: false, defaultValueSql: "GETUTCDATE()"),
+                    DateModified = table.Column<DateTime>(nullable: false, defaultValueSql: "GETUTCDATE()")
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_BeerStyle", x => x.StyleId);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "BeerStyle");
+        }
+    }
+}

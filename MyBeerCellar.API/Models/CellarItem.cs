20665ac8635d5b1cc3ec8b947d4645a3d1049a23
Christopher House <chhouse@microsoft.com>
2020-09-21 13:53:38 -0500

    Added cellaritem

---
+++ MyBeerCellar.API/Models/CellarItem.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace MyBeerCellar.API.Models
+{
+    public class CellarItem
+    {
+        public int CellarItemId { get; set; }
+
+        public string ItemName { get; set; }
+
+        public int YearProduced { get; set; }
+
+        public int Quantity { get; set; }
+
+        public DateTime DateCreated { get; set; }
+
+        public DateTime DateModified { get; set; }
+    }
+}
---
+++ MyBeerCellar.API/Models/Configuration/CellarItemConfiguration.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace MyBeerCellar.API.Models.Configuration
+{
+    public class CellarItemConfiguration : IEntityTypeConfiguration<CellarItem>
+    {
+        public void Configure(EntityTypeBuilder<CellarItem> builder)
+        {
+            builder.ToTable(nameof(CellarItem), Constants.DataConfiguration.SchemaName);
+
+            builder.HasKey(_ => _.CellarItemId);
+
+            builder.Property(_ => _.ItemName)
+                .HasMaxLength(255)
+                .IsRequired();
+
+            builder.Property(_ => _.YearProduced)
+                .IsRequired();
+
+            builder.Property(_ => _.Quantity)
+                .IsRequired();
+
+            builder.Property(_ => _.DateCreated)
+                .IsRequired()
+                .HasDefaultValueSql(Constants.DataConfiguration.CurrentUtcDateTimeDefault);
+
+            builder.Property(_ => _.DateModified)
+                .IsRequired()
+                .HasDefaultValueSql(Constants.DataConfiguration.CurrentUtcDateTimeDefault);
+
+            builder.HasIndex(_ => new {_.ItemName, _.YearProduced})
+                .IsUnique();
+        }
+    }
+}

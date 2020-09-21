063829bc473fd39dce14893e0dab45cbccc2ba54
Christopher House <chhouse@microsoft.com>
2020-09-21 17:23:08 -0500

    Added validation to style api

---
+++ MyBeerCellar.API/Validators/ViewModels/UpdateBeerStyleValidator.cs
@@ -0,0 +1,18 @@
+﻿using FluentValidation;
+using MyBeerCellar.API.ViewModels;
+
+namespace MyBeerCellar.API.Validators.ViewModels
+{
+    public class UpdateBeerStyleValidator : AbstractValidator<UpdateBeerStyle>
+    {
+        public UpdateBeerStyleValidator()
+        {
+            RuleFor(_ => _.StyleId)
+                .NotEmpty();
+
+            RuleFor(_ => _.StyleName)
+                .NotEmpty()
+                .MaximumLength(100);
+        }
+    }
+}

444cf9b7dd969debc41216c50b24d62e9a082d99
Christopher House <chhouse@microsoft.com>
2020-09-21 11:58:55 -0500

    Added initial methods for beercontainer api

---
+++ MyBeerCellar.API/Controllers/BeerContainerController.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.VisualBasic;
+using MyBeerCellar.API.Data;
+using MyBeerCellar.API.Models;
+
+namespace MyBeerCellar.API.Controllers
+{
+    public class BeerContainerController : BaseApiController
+    {
+        private readonly MyBeerCellarContext _context;
+
+        public BeerContainerController(MyBeerCellarContext context)
+        {
+            _context = context ?? throw new ArgumentNullException(nameof(context));
+        }
+
+        [HttpGet]
+        [ProducesResponseType(typeof(IEnumerable<BeerContainer>), StatusCodes.Status200OK)]
+        public async Task<IEnumerable<BeerContainer>> GetAsync()
+        {
+            var containers = await _context.BeerContainer.ToListAsync();
+
+            return containers;
+        }
+
+        [HttpGet]
+        [Route("/api/[controller]/{id}")]
+        [ProducesResponseTypeAttribute(typeof(BeerContainer), StatusCodes.Status200OK)]
+        public async Task<IActionResult> GetByIdAsync(int id)
+        {
+            IActionResult result = null;
+
+            var container = await _context.BeerContainer.FindAsync(id);
+
+            if (container != null)
+            {
+                result = Ok(container);
+            }
+            else
+            {
+                result = NotFound();
+            }
+
+            return result;
+        }
+    }
+}

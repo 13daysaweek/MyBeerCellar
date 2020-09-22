e191687db88293f3c55fac447679431023b7144d
Christopher House <chhouse@microsoft.com>
2020-09-22 13:34:34 -0500

    Added unit test porject + stub test

---
+++ MyBeerCellar.API.UnitTests/Class1.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using FluentAssertions;
+using Xunit;
+
+namespace MyBeerCellar.API.UnitTests
+{
+    public class Class1
+    {
+        [Fact]
+        public void Test_True()
+        {
+            true.Should()
+                .BeTrue();
+        }
+    }
+}
